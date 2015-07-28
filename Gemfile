54939068a6d912eac3a5375edd3049d4ed7f4381
Mark Wunsch <mwunsch@renttherunway.com>
2015-07-28 17:38:21 -0400

    My beautiful hand-rolled OAuth client isn't working
    Remove tumblr_client gem in favor of something more bespoke

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source "https://rubygems.org"
 
 gem 'github-pages'
-gem 'tumblr_client'
-
+gem 'simple_oauth'
--- _plugins/publish_to_tumblr.rb
+++ _plugins/publish_to_tumblr.rb
@@ -1,10 +1,10 @@
 require 'jekyll'
-require 'tumblr_client'
 require 'net/http'
 require 'uri'
 require 'json'
 require 'securerandom'
 require 'pp'
+require 'simple_oauth'
 
 class PublishToTumblr < Jekyll::Generator
   def generate(site)
@@ -19,21 +19,19 @@ class PublishToTumblr < Jekyll::Generator
     tumblelog.select(&:published?).reject {|p| p.data.key?("tumblr_id") }.each do |post|
       # Publish the post to Tumblr according to the post type
       puts "Publishing #{post.id}"
+      post_defaults = {
+        date: post.date,
+        slug: post.slug,
+        format: markdown?(site, post) ? "markdown" : "html",
+        # See: https://groups.google.com/forum/#!topic/tumblr-api/2E_rGjl9PE4
+        source_url: "#{site.config['url']}#{post.url}",
+        state: post.is_a?(Jekyll::Draft) ? "draft" : "queue" # Guard against my own foolishness.
+      }
       response = case post.data["type"]
         when "photo", "quote", "link", "chat", "audio", "video"
           {}
         else # text
-          tumblr_client.publish({
-            title: post.data["title"],
-            body: post.to_s,
-            date: post.date,
-            slug: post.slug,
-            format: markdown?(site, post) ? "markdown" : "html",
-            # See: https://groups.google.com/forum/#!topic/tumblr-api/2E_rGjl9PE4
-            # source_url: "#{site.config['url']}#{post.url}",
-            # The tumblr_client gem does not allow this param, but maybe I can just craft the request myself
-            state: post.is_a?(Jekyll::Draft) ? "draft" : "queue" # Guard against my own foolishness.
-          })
+          tumblr_client.publish post_defaults.merge({ title: post.data["title"], body: post.to_s })
         end
       if response["status"] || !response.has_key?("id")
         abort "Encountered an error when attempting to publish to Tumblr. Aborting.\n\t#{response.to_json}"
@@ -75,17 +73,13 @@ class TumblrConnection
 
   def initialize
     @oauth = OAUTH_VARS.to_h
-    @client = Tumblr::Client.new({
-      consumer_key: @oauth["TUMBLR_CONSUMER_KEY"],
-      consumer_secret: @oauth["TUMBLR_CONSUMER_SECRET"],
-      oauth_token: @oauth["TUMBLR_OAUTH_TOKEN"],
-      oauth_token_secret: @oauth["TUMBLR_OAUTH_TOKEN_SECRET"]
-    })
     @authorization = self.authorization
   end
 
   def publish(hash)
-    @client.text(HOST, hash)
+    response = post(hash)
+    pp response.body
+    JSON.parse(response.body)["response"]
   end
 
   def info
@@ -97,19 +91,24 @@ class TumblrConnection
 
   def post(post_params)
     uri = POST_URI
-    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-      http.request request(uri, post_params)
+    req = request(POST_URI, post_params)
+    Net::HTTP.start(req.uri.host, req.uri.port, use_ssl: true) do |http|
+      http.request req
     end
   end
 
   def request(uri, params={})
     Net::HTTP::Post.new(uri).tap do |req|
-      auth_header = @authorization.merge({oauth_signature: signature(req, params)}).map do |k,v|
-        %Q(#{k}="#{escape(v)}")
-      end.join(", ")
-
       req.form_data = params
-      req["Authorization"] = "OAuth #{auth_header}"
+      # auth_header = @authorization.merge({oauth_signature: signature(req, params)}).sort.map do |k,v|
+      #   %Q(#{k}="#{escape(v)}")
+      # end.join(", ") #My OAuth isn't working :-(
+      req["Authorization"] = SimpleOAuth::Header.new(req.method, req.uri, params, {
+        consumer_key: @oauth["TUMBLR_CONSUMER_KEY"],
+        token: @oauth["TUMBLR_OAUTH_TOKEN"],
+        consumer_secret: @oauth["TUMBLR_CONSUMER_SECRET"],
+        token_secret: @oauth["TUMBLR_OAUTH_TOKEN_SECRET"]
+      }).to_s
     end
   end
 
@@ -125,12 +124,12 @@ class TumblrConnection
   end
 
   def signature(req, params={})
-    normalized_params = @authorization.merge(params).sort.map{|k,v| "#{k}=#{v}" }.join("&")
+    normalized_params = @authorization.merge(params).sort.map{|k,v| "#{k}=#{escape(v)}" }.join("&")
     base = [req.method, req.uri.to_s, normalized_params].map do |item|
       escape(item)
     end.join("&")
     digest = OpenSSL::HMAC.digest(OpenSSL::Digest::SHA1.new, secret, base)
-    Base64.urlsafe_encode64(digest)
+    Base64.strict_encode64(digest)
   end
 
   def secret
