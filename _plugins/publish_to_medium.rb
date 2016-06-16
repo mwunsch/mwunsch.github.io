a824667b5e37913eb7303240aa0335c2f87cfd98
Mark Wunsch <mark@markwunsch.com>
2016-06-16 15:35:14 -0400

    Establish `publishable` method and use `next` in block

--- _plugins/publish_to_medium.rb
+++ _plugins/publish_to_medium.rb
@@ -1,13 +1,18 @@
 require 'jekyll'
 
 Jekyll::Hooks.register :documents, :pre_render do |post|
-  if PublishToMedium.suitable?(post)
-    p post.data["slug"]
+  next unless PublishToMedium.publishable?(post)
+  begin
+    puts "Publishing #{post.id}"
   end
 end
 
 class PublishToMedium
   def self.suitable?(post)
     post.data["categories"].include?("tumblelog") && ["text", nil].include?(post.data["type"])
+  end
+
+  def self.publishable?(post)
+    suitable?(post) && !post.data.key?("tumblr_id")
   end
 end
